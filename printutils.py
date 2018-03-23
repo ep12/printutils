253f911eae51c2c4ecb1046587bbd5ed9e23c1d8
ep12 <epsilonproducts12@gmail.com>
2018-03-23 19:31:47 +0100

    Version 1.0.0. Major bug fixes, feature enhancements...

--- printutils.py
+++ printutils.py
@@ -8,7 +8,7 @@ __email__ = ''
 __license__ = 'MIT'
 __title__ = 'printutils'
 __url__ = 'https://github.com/ep12/printutils'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 import sys
 from inspect import currentframe, getmro, isclass, isfunction
@@ -16,11 +16,25 @@ import regex
 
 returnFormatted = False
 outf = print
-eeregex = '(?P<todo>((^|[^\\\\])(\$|!)([^() ]+|(?<rec>\((?:[^()]++|(?&rec))*\))))|(^|[^\\\\])({.+}))'
+eeregex = '(?P<todo>' \
+	+ '((^|[^\\\\])(\$|!)((\w+[\w\d=-]*)|(?<rec>\((?:[^()]++|(?&rec))*\))))' \
+	+ '|(^|[^\\\\])({[^}]+}))'
 outopt = {'end': ''}
 allowExec = False
 warnOnDenial = True
 returnOnDenial = False
+warnOnError = True
+
+_fmtspec = '((?P<fill>.?)(?P<align>[0<>\\^]))?' \
+	+ '(?P<sign>[ +-])?' \
+	+ '(?P<altform>[#])?' \
+	+ '(?P<width>[\d]+)?' \
+	+ ',?(\\.(?P<precision>[\d]+))?' \
+	+ '(?P<type>[bcdeEfFgGnosxX])?'
+_fmtregex = '(?P<fieldname>[\d\w]+)' \
+	+ '(\\.(?P<attrname>\w*)|(\[(?P<elindex>[^\\]]*)\]))?' \
+	+ '(!(?P<conv>[rsa]))?' \
+	+ '(:' + _fmtspec + ')?'
 
 
 def printf(fmt: str, *args, **kwargs) -> str:
@@ -42,13 +56,16 @@ def printf(fmt: str, *args, **kwargs) -> str:
 	d.update(dl)
 	tmp = fmt
 	matches = [x[0] for x in regex.findall(eeregex, tmp, flags=regex.VERBOSE + regex.MULTILINE)]
-	# TODO: {eval} and $eval and !exec
+	# print('TODO matches:\n%s' % matches)  # DEBUG
 	for m in matches:
+		if m.startswith('\\'):
+			print('ERROR: %r' % m)
 		if not (m.startswith('$') or m.startswith('!') or m.startswith('{')):
 			m2 = m[1:]
 		else:
 			m2 = m
 		# print(repr(m), repr(m2))  # DEBUG
+		# print('\n', tmp, m, m2)
 		if m2.startswith('!'):
 			# print('exec(%r)' % m2)  # DEBUG
 			if allowExec:
@@ -63,7 +80,7 @@ def printf(fmt: str, *args, **kwargs) -> str:
 					return False
 				r = '[EXECUTION DENIED]' * warnOnDenial
 		elif m2.startswith('$'):
-			if m.startswith('$(') and m.endswith(')'):
+			if m2.startswith('$(') and m.endswith(')'):
 				m3 = m2[2:-1]
 			else:
 				m3 = m2[1:]
@@ -71,11 +88,20 @@ def printf(fmt: str, *args, **kwargs) -> str:
 			r = str(eval(m3, dg, dl))
 		elif m2.startswith('{') and m2.endswith('}'):  # BUG: Will not work on {0!r: <7} etc.
 			# print('eval(%r)' % m2[1:-1]) # DEBUG
-			r = str(eval(m2[1:-1], dg, dl))
+			m3 = m2[1:-1]
+			if bool(regex.fullmatch(_fmtregex, m3)):
+				try:
+					r = ('{%s}' % m3).format(*args, **d)
+				except IndexError:
+					r = '[FAILED]' * warnOnError
+			else:
+				r = str(eval(m3, dg, dl))
 		else:
 			r = ''
-		tmp = tmp.replace(m2, r)
-	tmp = tmp.format(fmt, *args, **d)
+		# print('m2: %r, %r -> %r' % (m2, m, m[0] * (m2 != m) + r))
+		tmp = tmp.replace(m, m[0] * (m2 != m) + r)
+	# tmp = tmp.format(fmt, *args, **d)
+	tmp = tmp.replace('\$', '$').replace('\!', '!').replace('\{', '{')
 	outf(tmp, **outopt)
 	if returnFormatted:
 		return tmp
