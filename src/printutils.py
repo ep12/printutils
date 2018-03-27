aaee555512dfddd417050d5fb0da5e0b2d1e226a
ep12 <epsilonproducts12@gmail.com>
2018-03-27 11:38:55 +0200

    major structure change

--- src/printutils.py
+++ src/printutils.py
@@ -36,40 +36,9 @@ import os as _os
 import sys as _sys
 import platform as _platform
 import time as _time
-import utils as _u
+import utils as ut
 # import colorama as _colorama
 # _get = None
-
-
-def _fdconv(fd, target=None):
-	if target is not None and isinstance(target, str):
-		if target.lower() in ['string', 's']:
-			if not isinstance(fd, str):
-				return _fdconv(fd)
-			else:
-				return fd
-		else:
-			if isinstance(fd, str):
-				return _fdconv(fd)
-			else:
-				return fd
-	if isinstance(fd, str):
-		if 'stdin' in fd.lower():
-			return _sys.stdin
-		if 'stdout' in fd.lower():
-			return _sys.stdout
-		if 'stderr' in fd.lower():
-			return _sys.stderr
-		else:
-			raise ValueError("fd not in ['stdin', 'stdout', 'stderr']!")
-	elif fd is _sys.stdin:
-		return 'stdin'
-	elif fd is _sys.stdout:
-		return 'stdout'
-	elif fd is _sys.stderr:
-		return 'stderr'
-	else:
-		raise ValueError('fd must be a string or a file descriptor!')
 
 
 _retfmt_dbg = '(fmt, dest, matches, errs, ret)'
@@ -77,9 +46,10 @@ retfmt = [None, _retfmt_dbg][hasattr(_sys.stdout, 'isatty') and _sys.stdout.isat
 outopt = {'end': ''}
 allowExec = False
 warnOnFail = True
-debug = False
+ut.debug = False
+dbg = ut.dbg
 
-useColors = {'stdout': False, 'stderr': False, 'all': False, 'any': False, 'format': False}
+useColors = ut.initColors()
 
 _eeregex = '(?P<todo>' \
 	+ '((^|[^\\\\])(\$|!)((\w+[\w\d=-]*)|(?<rec>\((?:[^()]++|(?&rec))*\))))' \
@@ -109,32 +79,6 @@ _fmtregex = '(?P<fieldname>[\d\w]+)' \
 # 			except ImportError:
 # 				print('Getch is required to read the cursor position.\nMake sure that you have it installed: pip3 install -U getch.')
 # 				system(1)
-
-
-def initColors():
-	'''initColors()
-	Tests if colors are supported by the device / terminal and sets the useColors options accordingly.
-	'''
-	for fd in [_sys.stdout, _sys.stderr]:
-		if (hasattr(fd, 'isatty') and fd.isatty()) or ('TERM' in _os.environ and _os.environ['TERM'] is 'ANSI'):
-			if _platform.system() is 'Windows':
-				if 'CONEMUANSI' in _os.environ and _os.environ['CONEMUANSI'] == 'ON':
-					useColors[_fdconv(fd)] = True
-				elif 'TERM' in _os.environ and _os.environ['TERM'] == 'ANSI':
-					useColors[_fdconv(fd)] = True
-				else:
-					useColors[_fdconv(fd)] = False
-			else:
-				useColors[_fdconv(fd)] = True
-		else:
-			useColors[_fdconv(fd)] = False
-
-	useColors['all'] = useColors['stdout'] and useColors['stderr']
-	useColors['any'] = useColors['stdout'] or useColors['stderr']
-	useColors['format'] = useColors['all']
-
-
-initColors()
 
 
 def colorSettings():
@@ -283,116 +227,6 @@ _sgr = {
 	'turquoisebg': 106,
 	'whitebg': 107
 }
-
-
-def _getCursor():
-	if _sys.platform == 'win32':
-		raise NotImplementedError('Feature not available')
-	appendmode = False
-	tmp = ''
-	_sys.stdout.buffer.write(_ansi['DSR'].encode())
-	_sys.stdout.buffer.flush()
-	while True:
-		try:
-			# if _sys.platform is 'win32':
-			# 	oprintf('[bluefg;whitebg] INFO [reset] Trying to detect cursor postion. Press enter:\r')
-			# 	tmp = _sys.stdin.readline()  # Windows
-			# 	oprintf(_ansi['EL'] % 0)
-			# 	break
-			# else:
-			x = _u.getch(0.01)
-		except KeyboardInterrupt:
-			tmp = x = ''
-		if x is '\x1b':
-			tmp = x
-			appendmode = True
-		elif appendmode:
-			tmp += x
-		if x in ['R', '\n', '']:
-			break
-	return tmp
-
-
-def CUP(row=None, col=None):
-	'''CUP(row=None, col=None)
-		If row and col are None, this function returns a tuple with the cursor position.
-		Otherwise any value that is None is set to 1 and the function sets the cursor position.
-	'''
-	if row is None and col is None:
-		try:
-			r = _getCursor()
-		except NotImplementedError:
-			r = ''
-		if r:
-			return tuple([int(x) for x in r[2:-1].split(';')])
-		else:
-			return (False, False)
-	else:
-		if row is None:
-			row = 1
-		if col is None:
-			col = 1
-		assert isinstance(row, int) and row > 0, 'row must be a positive integer'
-		assert isinstance(col, int) and col > 0, 'col must be a positive integer'
-		_sys.stdout.buffer.write((_ansi['CUP'] % (row, col)).encode())
-
-
-def _OSC_color_available(n: int=0) -> (bool, str, tuple):
-	assert isinstance(n, int) and n >= 0 and n < 256
-	print('_OSC_available(%d)' % n)
-	if _sys.platform in ['darwin', 'linux']:
-		s = _time.time()
-		appendmode = False
-		tmp = ''
-		_sys.stdout.buffer.write(('\x1b]4;%d;?\a' % n).encode())
-		_sys.stdout.buffer.flush()
-		while True:
-			x = _u.getch(0.1, True)  # BUG: Doesn't work on windows
-			# x = _u.getch(-1, True)
-			print(x)
-			if x is '\x1b':
-				tmp = x
-				appendmode = True
-			elif appendmode:
-				tmp += x
-			if x in ['\a', '\n', '']:
-				break
-		if tmp not in ['\n', '']:
-			print(repr(tmp))
-			try:
-				m = _re.match('\x1b]4;[\\d]+;rgb:(\d{2})(\d{2})/(\d{2})(\d{2})/(\d{2})(\d{2})\x07', tmp).groups()
-				rgb = (int(m[0]) << 4 + int(m[1]), int(m[2]) << 4 + int(m[3]), int(m[4]) << 4 + int(m[5]))
-			except Exception:
-				rgb = (0x00, 0x00, 0x00)
-			return (True, tmp, rgb)
-	return (False, None, (0x00, 0x00, 0x00))
-
-
-def getColorRGB(n: int=0) -> tuple:
-	success, dummy, rgb = _OSC_color_available(n)
-	if success:
-		return rgb
-
-
-def testBuiltinColors() -> int:
-	'''testBuiltinColors()
-	returns the number of supported colors or -1 if terminal does not support the detection.
-	'''
-	if not _OSC_available()[0]:
-		return -1
-	mn, mx = 0, 256
-	x = 0
-	while x + 1 < mx:
-		n = _time.time()
-		oprintf('\r' + ((int(4 * n) % 2) * ' ' + '.').ljust(3) + 3 * '\b')
-		x = (mn + mx) // 2
-		r = _OSC_available(x)[0]
-		if r:
-			mn = x
-		else:
-			mx = x
-	oprintf('   \r')
-	return x
 
 
 def _extracolor(j):  # TODO!!!!!!!!!!!!!!!
@@ -639,12 +473,6 @@ def eprintf(fmt: str, *args, **kwargs):
 	'''
 	frame = kwargs.pop('_frame_', currentframe().f_back)
 	_printf(fmt, 'stderr', frame, *args, **kwargs)
-
-
-def dbg(*args):
-	if debug:
-		print('DEBUG: ', end='')
-		print(*args)
 
 
 def hook(sysobj):
--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,12 @@ def pipimport(name: str):
 		print('Error: could not import %r!\nInstall using \'pip install -U %s\'.' % (name, name))
 
 
+def dbg(*args):
+	if debug:
+		print('DEBUG: ', end='')
+		print(*args)
+
+
 if _sys.platform == 'linux':
 	_g = pipimport('getch')
 elif _sys.platform is 'win32':
@@ -75,3 +81,170 @@ def getBuffer(visible: bool=False):
 			return tmp
 		else:
 			tmp += x
+
+
+def fdconv(fd, target=None):
+	if target is not None and isinstance(target, str):
+		if target.lower() in ['string', 's']:
+			if not isinstance(fd, str):
+				return _fdconv(fd)
+			else:
+				return fd
+		else:
+			if isinstance(fd, str):
+				return _fdconv(fd)
+			else:
+				return fd
+	if isinstance(fd, str):
+		if 'stdin' in fd.lower():
+			return _sys.stdin
+		if 'stdout' in fd.lower():
+			return _sys.stdout
+		if 'stderr' in fd.lower():
+			return _sys.stderr
+		else:
+			raise ValueError("fd not in ['stdin', 'stdout', 'stderr']!")
+	elif fd is _sys.stdin:
+		return 'stdin'
+	elif fd is _sys.stdout:
+		return 'stdout'
+	elif fd is _sys.stderr:
+		return 'stderr'
+	else:
+		raise ValueError('fd must be a string or a file descriptor!')
+
+
+def initColors():
+	'''initColors()
+	Tests if colors are supported by the device / terminal and sets the useColors options accordingly.
+	'''
+	useColors = {'stdout': False, 'stderr': False, 'all': False, 'any': False, 'format': False}
+	for fd in [_sys.stdout, _sys.stderr]:
+		if (hasattr(fd, 'isatty') and fd.isatty()) or ('TERM' in _os.environ and _os.environ['TERM'] is 'ANSI'):
+			if _platform.system() is 'Windows':
+				if 'CONEMUANSI' in _os.environ and _os.environ['CONEMUANSI'] == 'ON':
+					useColors[_u.fdconv(fd)] = True
+				elif 'TERM' in _os.environ and _os.environ['TERM'] == 'ANSI':
+					useColors[_u.fdconv(fd)] = True
+				else:
+					useColors[_u.fdconv(fd)] = False
+			else:
+				useColors[_u.fdconv(fd)] = True
+		else:
+			useColors[_u.fdconv(fd)] = False
+
+	useColors['all'] = useColors['stdout'] and useColors['stderr']
+	useColors['any'] = useColors['stdout'] or useColors['stderr']
+	useColors['format'] = useColors['all']
+	return useColors
+
+
+def _getCursor():
+	if _sys.platform == 'win32':
+		raise NotImplementedError('Feature not available')
+	appendmode = False
+	tmp = ''
+	_sys.stdout.buffer.write(_ansi['DSR'].encode())
+	_sys.stdout.buffer.flush()
+	while True:
+		try:
+			# if _sys.platform is 'win32':
+			# 	oprintf('[bluefg;whitebg] INFO [reset] Trying to detect cursor postion. Press enter:\r')
+			# 	tmp = _sys.stdin.readline()  # Windows
+			# 	oprintf(_ansi['EL'] % 0)
+			# 	break
+			# else:
+			x = _u.getch(0.01)
+		except KeyboardInterrupt:
+			tmp = x = ''
+		if x is '\x1b':
+			tmp = x
+			appendmode = True
+		elif appendmode:
+			tmp += x
+		if x in ['R', '\n', '']:
+			break
+	return tmp
+
+
+def cursorPosition(row=None, col=None):
+	'''cursorPosition(row=None, col=None)
+		If row and col are None, this function returns a tuple with the cursor position.
+		Otherwise any value that is None is set to 1 and the function sets the cursor position.
+	'''
+	if row is None and col is None:
+		try:
+			r = _getCursor()
+		except NotImplementedError:
+			r = ''
+		if r:
+			return tuple([int(x) for x in r[2:-1].split(';')])
+		else:
+			return (False, False)
+	else:
+		if row is None:
+			row = 1
+		if col is None:
+			col = 1
+		assert isinstance(row, int) and row > 0, 'row must be a positive integer'
+		assert isinstance(col, int) and col > 0, 'col must be a positive integer'
+		_sys.stdout.buffer.write((_ansi['CUP'] % (row, col)).encode())
+
+
+def OSC_color_available(n: int=0) -> (bool, str, tuple):
+	assert isinstance(n, int) and n >= 0 and n < 256
+	if _sys.platform in ['darwin', 'linux']:
+		s = _time.time()
+		appendmode = False
+		tmp = ''
+		_sys.stdout.buffer.write(('\x1b]4;%d;?\a' % n).encode())
+		_sys.stdout.buffer.flush()
+		while True:
+			x = ut.getch(0.1, True)  # BUG: Doesn't work on windows
+			# x = ut.getch(-1, True)
+			print(repr(x))
+			if len(x) is 0:
+				break
+			if x is '\x1b':
+				tmp = x
+				appendmode = True
+			elif appendmode:
+				tmp += x
+			if x in ['\a', '\n', '']:
+				break
+		if tmp not in ['\n', '']:
+			print(repr(tmp))
+			try:
+				m = _re.match('\x1b]4;[\\d]+;rgb:(\d{2})(\d{2})/(\d{2})(\d{2})/(\d{2})(\d{2})\x07', tmp).groups()
+				rgb = (int(m[0]) << 4 + int(m[1]), int(m[2]) << 4 + int(m[3]), int(m[4]) << 4 + int(m[5]))
+			except Exception:
+				rgb = (0x00, 0x00, 0x00)
+			return (True, tmp, rgb)
+	return (False, None, (0x00, 0x00, 0x00))
+
+
+def getColorRGB(n: int=0) -> tuple:
+	success, dummy, rgb = _OSC_color_available(n)
+	if success:
+		return rgb
+
+
+def testBuiltinColors() -> int:
+	'''testBuiltinColors()
+	returns the number of supported colors or -1 if terminal does not support the detection.
+	'''
+	if not _OSC_available()[0]:
+		return -1
+	mn, mx = 0, 256
+	x = 0
+	while x + 1 < mx:
+		n = _time.time()
+		oprintf('\r' + ((int(4 * n) % 2) * ' ' + '.').ljust(3) + 3 * '\b')
+		x = (mn + mx) // 2
+		r = _OSC_available(x)[0]
+		if r:
+			mn = x
+		else:
+			mx = x
+	oprintf('   \r')
+	return x
